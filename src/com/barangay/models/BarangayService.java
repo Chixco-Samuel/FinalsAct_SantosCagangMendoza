3b1d1a3f85ed00cdfb1aff10bfbc9d7e1dec28d3
PROGLAB-08 <PROGLAB-08@BJSB.LAB>
2026-06-25 15:25:28 +0800

    Add getters and setters for BarangayService fields

--- src/com/barangay/models/BarangayService.java
+++ src/com/barangay/models/BarangayService.java
@@ -1,5 +1,41 @@
 package com.barangay.models;
 
-public abstract class BarangayService {
-    
-}
+public class BarangayService {
+     private String serviceId;
+    private String serviceName;
+    private String description;
+
+    public BarangayService(String serviceId, String serviceName, String description) {
+        this.serviceId = serviceId;
+        this.serviceName = serviceName;
+        this.description = description;
+ 
+    }
+
+     public String getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(String serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+
