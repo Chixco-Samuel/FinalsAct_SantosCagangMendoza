6df3acde4a7beeec42ac7e053d1e6afc04f1cce5
Chixco-Samuel <chico.santos.college@ustangelicum.edu.ph>
2026-06-26 23:25:51 +0800

    Added searching in ResidentService

--- src/com/barangay/services/ResidentService.java
+++ src/com/barangay/services/ResidentService.java
@@ -4,10 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ResidentService {
-
     private List<Resident> residents;
 
     public ResidentService() {
         residents = new ArrayList<>();
+    }
+
+    public void registerResident(Resident resident) {
+        residents.add(resident);
+    }
+
+    public Resident findResidentById(String id) {
+        for (Resident resident : residents) {
+            if (resident.getId().equals(id)) {
+                return resident;
+            }
+        }
+        return null;
     }
 }
