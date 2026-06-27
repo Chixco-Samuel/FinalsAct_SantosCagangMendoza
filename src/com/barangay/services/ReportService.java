14a664887147a6b939137498f962ab6b232befa1
MMLAB-01 <MMLAB-01@BJSB.LAB>
2026-06-27 15:48:28 +0800

    Add ReportService implementation

--- src/com/barangay/services/ReportService.java
+++ src/com/barangay/services/ReportService.java
@@ -1,5 +1,13 @@
-package services;
+package com.barangay.services;
 
-public class ReportService {
+import com.barangay.interfaces.Reportable;
+import com.barangay.models.PaymentTransaction;
+import java.util.List;
+
+public class ReportService implements Reportable {
+    private PaymentService paymentService;
+
+    public ReportService(PaymentService paymentService) {
+        this.paymentService = paymentService;
     
 }
